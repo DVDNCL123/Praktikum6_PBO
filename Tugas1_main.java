de6c7cd1a4e16328001d7d876c04cab77847a1bc
DVDNCL123 <164830789+DVDNCL123@users.noreply.github.com>
2024-10-14 23:12:12 +0700

    Add files via upload

---
+++ Tugas1_main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Praktikum_Bab6;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tugas1_main {
+    public static void main(String[] args) {
+  
+        Tugas1 perhitungan = new Tugas1();
+
+        perhitungan.itungPrima();
+
+        perhitungan.deretGanjilGenap();
+
+        perhitungan.deretHurufZA();
+
+        perhitungan.laguAnakAyam();
+    }
+}
